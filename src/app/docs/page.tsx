525e74e8bdb9e5b7b1e463017cdd360be056fd95
Travis Vachon <travis.vachon@gmail.com>
2023-10-09 17:26:11 +0100

    fix: lint

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -5,7 +5,7 @@ export default function Docs () {
       <div className='flex flex-col justify-start items-center min-h-full w-full text-left text-white p-8'>
         <h1 className='text-3xl mb-8 font-bold'>Welcome to the w3up web console beta!</h1>
         <p className='mb-4 w-4/5'>
-          We're excited that you're using the w3up web console. This is a web-based interface
+          We`&apos;`re excited that you`&apos;`re using the w3up web console. This is a web-based interface
           to <Link href='https://github.com/web3-storage/w3up'>w3up</Link>, designed to make it easy
           for you to upload data to the Filecoin network.
         </p>
@@ -25,7 +25,7 @@ export default function Docs () {
         </ul>
         <p className='mb-4 w-4/5'>
           Otherwise, visit the <Link href='https://github.com/web3-storage/w3up'>w3up</Link> repository
-          where you'll find documentation and source code for the w3up protocol. You may
+          where you`&apos;`ll find documentation and source code for the w3up protocol. You may
           also be interested in reading the <Link href='https://github.com/web3-storage/specs'>w3up specifications</Link> and
           learning more about <Link href='https://github.com/web3-storage/w3infra'>the operational infrastructure code</Link> that
           powers our implementation of w3up.
