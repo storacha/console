eb2a3aaa425b7a0ff19eb55aa2b8a1f756824c13
Travis Vachon <travis.vachon@protocol.ai>
2023-12-01 11:34:11 -0800

    fix: get logout page working again (#50)
    This will work once https://github.com/web3-storage/w3ui/pull/595 is
    released.

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,19 +1,16 @@
 'use client'
-//import { useW3 } from "@w3ui/react"
+import { useW3 } from "@w3ui/react"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function LogoutPage () {
-  //const [, { unloadAgent }] = useW3()
+  const [, { logout }] = useW3()
 
-  function unloadAgent(){
-    // TODO need to figure out how this will work with the new library
-  }
   const router = useRouter()
   useEffect(function () {
-    if (unloadAgent) {
+    if (logout) {
       async function logOutAndRedirect () {
-        await unloadAgent()
+        await logout()
         router.push('/')
       }
       logOutAndRedirect()
