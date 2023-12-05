b7fbf27dc4fde85708b70820034e1d6dc8e13d74
Alan Shaw <alan.shaw@protocol.ai>
2023-12-05 00:19:35 +0000

    feat: simple visualization of inclusion proof

--- src/app/space/[did]/root/[cid]/shard/[shard]/page.tsx
+++ src/app/space/[did]/root/[cid]/shard/[shard]/page.tsx
@@ -6,11 +6,13 @@ import useSWR from 'swr'
 import { parse as parseLink } from 'multiformats/link'
 import DefaultLoader from '@/components/Loader'
 import * as Claims from '@web3-storage/content-claims/client'
-import { Piece, PieceLink } from '@web3-storage/data-segment'
+import { Aggregate, MerkleTreeNode, Piece, PieceLink, ProofData } from '@web3-storage/data-segment'
+import { Proof } from '@web3-storage/data-segment'
 import CopyIcon from '@/components/CopyIcon'
 import { EqualsClaim } from '@web3-storage/content-claims/client/api'
 import { Breadcrumbs } from '@/components/Breadcrumbs'
 import { filesize } from '@/lib'
+import archy from 'archy'
 
 interface PageProps {
   params: {
@@ -67,6 +69,7 @@ export default function ItemPage ({ params }: PageProps): JSX.Element {
       if (out.error) {
         throw new Error('failed to get filecoin info', { cause: out.error })
       }
+      console.log(out.ok.aggregates)
       return out.ok
     },
     onError: err => console.error(err.message, err.cause)
@@ -103,14 +106,20 @@ export default function ItemPage ({ params }: PageProps): JSX.Element {
       <div className='pb-5 font-mono text-sm overflow-hidden no-wrap text-ellipsis'>
         {claim.isLoading || filecoinInfo.isLoading
           ? <DefaultLoader className='w-5 h-5 inline-block' />
-          : filecoinInfo.data?.aggregates.length
-            ? filecoinInfo.data?.aggregates.map(({ aggregate }) => {
+          : filecoinInfo.data && filecoinInfo.data.aggregates.length
+            ? filecoinInfo.data.aggregates.map(({ aggregate, inclusion }) => {
+                const piece = filecoinInfo.data?.piece
+                if (!piece) return <div />
                 return (
                   <div key={aggregate.toString()}>
                     {aggregate.toString()}<CopyIcon text={aggregate.toString()} />
                     <br/>
                     <span className='font-sans opacity-60'>aka </span>
                     {Piece.fromLink(aggregate).toInfo().link.toString()}<CopyIcon text={aggregate.toString()} />
+                    <H2 className='mt-5'>Inclusion Proof</H2>
+                    <pre className='font-mono text-xs'>
+                      {renderInclusionProof(inclusion.subtree, piece)}
+                    </pre>
                   </div>
                 )
               })
@@ -145,4 +154,49 @@ function isPieceLink(link: any): link is PieceLink {
   } catch {
     return false
   }
+}
+
+const MAX_DEPTH = 63
+
+// Adapted from https://github.com/web3-storage/data-segment/blob/e9cdcbf76232e5b92ae1d13f6cf973ec9ab657ef/src/proof.js#L62-L86
+function renderInclusionProof (proof: ProofData, pieceLink: PieceLink): string {
+  const piece = Piece.fromLink(pieceLink)
+
+  if (Proof.depth(proof) > MAX_DEPTH) {
+    throw new RangeError('merkle proofs with depths greater than 63 are not supported')
+  }
+
+  let position = BigInt(Proof.offset(proof))
+  if (position >> BigInt(Proof.depth(proof)) !== 0n) {
+    throw new RangeError('offset greater than width of the tree')
+  }
+
+  const { root } = piece
+  let nodes: archy.Data['nodes'] = []
+  let top = root
+  let right = 0n
+
+  for (const node of Proof.path(proof)) {
+    right =  position & 1n
+    position = position >> 1n
+
+    let label = `Uint8Array(${top.length}) [${top}]`
+    if (top === root) {
+      label = `${label}\n(${pieceLink})`
+    }
+
+    if (right === 1n) {
+      nodes = ['*', { label, nodes }]
+    } else {
+      nodes = [{ label, nodes }, '*']
+    }
+    top = right === 1n ? Proof.computeNode(node, top) : Proof.computeNode(top, node)
+  }
+
+  // Derive the aggregate tree height by adding depth of the proof tree to the
+  // height of the piece (sub)tree.
+  const height = piece.height + Proof.depth(proof)
+  const aggregate = Aggregate.toLink({ root: top, height })
+
+  return archy({ label: aggregate.toString(), nodes })
 }
