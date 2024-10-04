28a9d63cac7ac7ba2be3f9a9f685be4dd998fb2b
Petra Jaros <peeja@peeja.com>
2024-10-04 11:48:52 -0400

    WIP - devtoolsFormatters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-'use client'
-
-import { useW3, Space } from '@w3ui/react'
+'use client';
+import { useW3, Space, Principal } from '@w3ui/react'
 import { DidIcon } from '@/components/DidIcon'
 import Link from 'next/link'
 import { SpacesNav } from './space/layout'
 import { H1, H2 } from '@/components/Text'
 import SidebarLayout from '@/components/SidebarLayout'
 import { ReactNode } from 'react'
+import { CID } from 'multiformats/cid'
+import { Delegation } from '@ucanto/core/delegation';
 
 export default function HomePage () {
   return (
@@ -50,3 +51,75 @@ function Item ({space}: {space: Space}) {
     </Link>
   )
 }
+
+const idCss = `
+  padding: 0 0.25rem;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+`
+const idIconCss = `
+  font-size: 0.7em;
+`;
+
+
+const didCss = `
+  ${idCss}
+  background-color: hsla(300, 100%, 50%, 0.2);
+`;
+
+const cidCss = `
+  ${idCss}
+  background-color: hsla(49, 100%, 50%, 0.2);
+`;
+
+const isPrincipal = (obj: object): obj is Principal => "did" in obj && typeof obj.did === 'function';
+const isCID = (obj: object): obj is CID => "bytes" in obj && '/' in obj && typeof obj.bytes !== 'undefined' && obj['/'] === obj.bytes;
+
+if (typeof window !== 'undefined') {
+  // @ts-expect-error
+  window.devtoolsFormatters = [
+    {
+      header: function(obj: object) {
+        if (isPrincipal(obj)) {
+          return ["div", {style: didCss}, ["div", {style: idIconCss}, "🪪"], ["div", {}, obj.did()]]
+        } 
+      },
+      hasBody: function(obj: object) {
+        if (isPrincipal(obj)) {
+          return false
+        }
+      },
+    },
+    {
+      header: function(obj: object) {
+        if (isCID(obj)) {
+          return ["div", {style: cidCss}, ["div", {style: idIconCss}, "📜"], ["div", {}, obj.toString()]]
+        } 
+      },
+      hasBody: function(obj: object) {
+        if (isCID(obj)) {
+          return false
+        }
+      },
+    }
+  ]
+
+
+  // const blocksSymbol = Symbol('blocks')
+
+  // Object.defineProperty(Delegation.prototype, 'blocks', {
+  //   set: function(value) {
+  //     console.log('setting blocks', value)
+  //     this[blocksSymbol] = value
+  //   },
+  //   get: function() {
+  //     return this[blocksSymbol]
+  //   }
+  // })
+
+  // // @ts-expect-error
+  // window.Delegation = Delegation
+ 
+}
+
