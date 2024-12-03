47ba373de9fd17862b9625070c8a10d844b0f67d
Travis Vachon <travis.vachon@gmail.com>
2024-12-02 16:25:47 -0800

    chore: remove unused code

--- src/lib/referrals.ts
+++ src/lib/referrals.ts
@@ -1,13 +1,3 @@
-import { customAlphabet } from 'nanoid'
-
-// 16 characters from this alphabet is plenty safe enough - per https://zelark.github.io/nano-id-cc/
-// "399B IDs needed, in order to have a 1% probability of at least one collision."
-const REFCODE_LENGTH = 16
-// from https://github.com/CyberAP/nanoid-dictionary - "This list should protect you from accidentally getting obscene words in generated strings."
-const nanoid = customAlphabet("6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz")
-
-export const generateRefcode = () => nanoid(REFCODE_LENGTH)
-
 export const REFERRALS_SERVICE = process.env.NEXT_PUBLIC_REFERRALS_SERVICE_URL
 
 export async function createRefcode (form: FormData) {
