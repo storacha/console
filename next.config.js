a48c8c42cbd0dbba7902691f839aebac0b4a04b6
Travis Vachon <travis@storacha.network>
2024-12-19 01:49:04 +0100

    fix: add sentry auth token to enable sourcemap upload (#168)
    this will give us nice stack traces and other conveniences in Sentry

--- next.config.js
+++ next.config.js
@@ -15,6 +15,8 @@ const { withSentryConfig } = require("@sentry/nextjs");
 module.exports = withSentryConfig(
   module.exports,
   {
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
