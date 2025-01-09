16ffb75851d894a2cd37f034a20fa45f28a27b86
Felipe Forbeck <felipe.forbeck@gmail.com>
2025-01-09 20:44:31 -0300

    fix: cors preflight issue in production

--- src/components/services.ts
+++ src/components/services.ts
@@ -33,4 +33,9 @@ export const serviceConnection = connect<Service>({
   }),
 })
 
-export const gatewayHost = process.env.NEXT_PUBLIC_W3UP_GATEWAY_HOST ?? 'https://w3s.link'
+/**
+ * Using freeway.dag.haus/freeway-staging.dag.haus as the gateway host directly without going through w3s.link
+ * to avoid the need to avoid access fetch error in production due to the w3s.link CORS policy:
+ * - Redirect is not allowed for a preflight request.
+ */
+export const gatewayHost = process.env.NEXT_PUBLIC_W3UP_GATEWAY_HOST ?? 'https://freeway.dag.haus'
